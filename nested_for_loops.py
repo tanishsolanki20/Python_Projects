73754f3ac0a36bd849c82212f26bd09fe8aeb78b
tanishsolanki20 <tanish.solanki20@gmail.com>
2026-05-14 18:59:04 +0530

    Refactor nested loops to accept user input for range

--- nested_for_loops.py
+++ nested_for_loops.py
@@ -2,7 +2,10 @@
     for j in range(1, 4):
         print(i, j)"""
 
-a= ["Tanish", "Solanki"]
-for i, name in enumerate(a):
-    print(f"Index: {i}, {name}")
-    print(list(enumerate(a)))
+b= input("Enter a number: ")
+for i in range(1, int(b) + 1):
+    for j in range(1, int(b) + 1):
+        print(i, j)
+
+
+
