2681f0253a5ea65b17aec998673575144e84a4ce
tanishsolanki20 <tanish.solanki20@gmail.com>
2026-05-14 19:00:59 +0530

    Add enumeration example for list items in nested_for_loops.py

--- nested_for_loops.py
+++ nested_for_loops.py
@@ -2,6 +2,13 @@
     for j in range(1, 4):
         print(i, j)"""
 
+
+a= ["Tanish", "Solanki"]
+for i, name in enumerate(a):
+    print(f"Index: {i}, {name}")
+    print(list(enumerate(a)))
+
+    
 b= input("Enter a number: ")
 for i in range(1, int(b) + 1):
     for j in range(1, int(b) + 1):
