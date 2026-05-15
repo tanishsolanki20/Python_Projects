cc861bdcfc9000e6716e32b7fcea4168197dd680
tanishsolanki20 <tanish.solanki20@gmail.com>
2026-05-15 17:01:26 +0530

    Add user input prompt for tung tung sahur with response handling

---
+++ practice__2.py
@@ -0,0 +1,7 @@
+tung_tung_sahur=input("Do you like tung tung sahur? (yes/no) ")
+if tung_tung_sahur.lower() == "yes":
+    print("That's great! That means you are brainrot minded.")
+elif tung_tung_sahur.lower() == "no":
+    print("Oh, that's too bad. You might be missing out on some fun brainrot content!")
+else:
+    print("Invalid input. Please answer with 'yes' or 'no'.")
