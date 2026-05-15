2566b0f45149ead7d5cab5a353843b1faab28b63
tanishsolanki20 <tanish.solanki20@gmail.com>
2026-05-15 17:59:31 +0530

    Add initial revision.py file with various examples and exercises

---
+++ revision.py
@@ -0,0 +1,80 @@
+"""name="Tanish"
+age= 12
+print(age)
+print(name)
+print(f"My name is {name} and my age is {age}.")
+
+First_name = "Tanish"
+Last_name = "Solanki"
+Full_name = First_name + " " + Last_name #concatenation of two strings
+print(Full_name)
+print(len(Full_name)) 
+print(Full_name .upper())
+print(Full_name .lower())
+name2= input("what is your name :")
+print(f"Nice to meet you {name2}")"""
+"""animal= input("Enter an animal :")
+place= input("Enter name of a place :")
+action= input("Enter a action :")
+print(f"Once upon a time, there was a {animal} who lived in {place}. Every day, it would {action} and have fun!")"""
+a=15
+b=3
+"""print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b) #floor division
+print(a%b) #modulo operator
+print(a**b) #exponentiation"""
+"""num1= int(input("Enter a number :"))
+print(num1 + 10)"""
+"""print(10 > 5) #greater than
+print(10 < 5) #less than
+print(10 == 5) #equal to
+print(10 != 5) #not equal to
+print(10 >= 5) #greater than or equal to
+print(10 <= 5) #less than or equal to
+print(10 is 5) #identity operator"""
+"""age=int(input("Enter your age: "))
+if age >=18:
+    print ("You are allowed to vote.")
+else:
+    print ("Sorry, you cannot vote you must be atleast 18 years old.")"""
+"""marks= int(input("Enter your marks: "))
+if marks >=90 and marks<=100:
+    print("You've pased. Grade: A")
+elif marks >=75 and marks<=90:
+    print("You've passed. Grade: B")
+elif marks >=60 and marks<=75:
+    print("You've passed. Grade: C")
+elif marks >=40 and marks<=60:
+    print("You've passed. Grade: D")
+else:
+    print("You've failed. Grade: F")
+"""
+f"""or i in range(5):
+    print(i)
+for l in range(1, 6):
+    print(l)"""
+"""for i in range(0, 10, 2):
+    print (i)"""
+"""friends= ["Aditya", "Pratyush", "Dev"]
+for friend in friends:
+    print(f"Hello, {friend}")"""
+numbers= [1, 24, 23, 57]
+"""for num in numbers:
+    if num >10:
+        print(num)"""
+"""fruits= ["apple", "banana", "orange", "mango"]
+print(fruits[0])
+print(fruits[1])
+print(fruits[-1])
+print(len(fruits))"""
+scores= [100, 30, 24, 57, 35, 91]
+scores.append(67)
+scores.remove(67)
+print(max(scores))
+print(min(scores))
+scores.sort()
+print(scores)
+
