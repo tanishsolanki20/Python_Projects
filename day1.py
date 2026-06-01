aa0c23b7ecd90f58ce5cc621d7cbb7ccbe0fd4dd
tanishsolanki20 <tanish.solanki20@gmail.com>
2026-06-01 19:35:02 +0530

    Refactor day1.py to encapsulate code in docstrings; add check_answer function and Dog class with bark method

--- day1.py
+++ day1.py
@@ -1,4 +1,4 @@
-name="tanish"
+"""name="tanish"
 age=12
 tall=True
 
@@ -7,4 +7,32 @@ print(type(name))
 name2=input("What is your name ? ")
 print("Hello",name2)
 
-print(f"Your name is {name2}")
+print(f"Your name is {name2}")"""
+
+
+
+"""def check_answer(player_answer, correct_answer, current_score):
+    if player_answer.lower()== correct_answer.lower():
+        new_score = current_score + 10
+        feedback = "Correct! +10 points"
+    else:
+        new_score = current_score
+        feedback = "Wrong! The answer was: " + correct_answer
+
+    return False, new_score, feedback
+correct_answer="paris"
+player_answer="france"
+current_score=20
+correct, score, msg = check_answer(player_answer, correct_answer, current_score)
+print(msg)         
+print(score)  """
+class Dog:
+    def __init__(self, name, breed):
+        self.name = name
+        self.breed = breed
+
+    def bark(self):
+        return f"{self.name} says Woof!"
+
+my_dog = Dog("Bruno", "Labrador")  # object
+print(my_dog.bark())  # Bruno says Woof!
