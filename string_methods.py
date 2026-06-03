09bfa6b8f91d6d5976e69ecb5b8e422d980682c2
tanishsolanki20 <tanish.solanki20@gmail.com>
2026-06-03 09:59:59 +0530

    Add string manipulation examples and user input handling in string_methods.py

---
+++ string_methods.py
@@ -0,0 +1,23 @@
+"""answer= "paris"
+guess= input("Enter the capital of France: ")
+
+if guess.lower() == answer:
+    print("Correct!")
+else:
+    print("Wrong, try again.")"""
+name= "     tanish solanki      "
+print(name)
+#full_name= name.split()
+print(name.lstrip().rstrip())
+# print(full_name)
+# print(full_name[0])
+# print(full_name[1])
+first_name= "tanish"
+last_name= "solanki"
+print(name)
+print(name.upper())
+print(name.capitalize())
+
+print(first_name.capitalize()+" "+last_name.capitalize())
+
+print(name.title())
