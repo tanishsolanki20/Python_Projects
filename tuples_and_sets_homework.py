4c5e626d5ed08fbb7d7221de5125952754c1bd95
tanishsolanki20 <tanish.solanki20@gmail.com>
2026-06-22 16:08:37 +0530

    Add initial implementation of favorite movies, number comparison, and tag deduplicator examples

---
+++ tuples_and_sets_homework.py
@@ -0,0 +1,13 @@
+#Write a program that stores 5 of your favorite movies in a tuple (since you won't be changing this list), then prints them out one per line using a loop.
+
+"""favourite_movies= ("Spiderman", "Interstellar", "Superman", "Avengers", "Haunted House")
+for i in favourite_movies:
+    print(i)"""
+
+#Write a program that takes two lists of numbers from the user, converts both to sets, and prints which numbers appear in both lists.
+"""x=int(input("Enter a number: "))
+y=int(input("Enter another number: "))
+v={x, y}
+print(f"First you entered {x} and then you entered {y}")"""
+
+#Build a "tag deduplicator" — given a list of hashtags someone used across multiple posts (with repeats and inconsistent casing like "Python" vs "python"), normalize the casing and return only the unique tags as a set.
