4c3525a684c8fa348d4f2bffcc0b9e01ee2e6f51
tanishsolanki20 <tanish.solanki20@gmail.com>
2026-08-28 15:49:05 +0530

    Refactor min_max function and add greet function; create new settings for Python environment

--- day7.py
+++ day7.py
@@ -3,6 +3,10 @@
 print(min_max([3, 1, 4, 1, 5, 9]))
 print (min_max([33, 44]))
 """
-def min_max2(a, b):
-    return min(a, b), max(a, b)
-print(min_max2(5, 5))
+"""def my_user_id_and_password(password,user_id="Tanish@20"):
+    return password, user_id
+print(my_user_id_and_password("737373"))"""
+
+def greet(name,age=13):
+    return f"My name is {name} and my age is {age}"
+print(greet("Tanish"))
