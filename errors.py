bbfede732396f2224641a8a44b97d159291e8f9a
tanishsolanki20 <tanish.solanki20@gmail.com>
2026-09-04 17:24:24 +0530

    Remove commented-out code and fix indentation in errors.py

--- errors.py
+++ errors.py
@@ -1,8 +1,8 @@
-"""a= "wow"
+a= "wow"
 b= 3
 print(a+str(b)) #Type Error: can only concatenate str (not "int") to str
 
-print(name) #NameError: name 'name' is not defined"""
+print(name) #NameError: name 'name' is not defined
 
 
 #print("wow) #SyntaxError: EOL while scanning string literal
