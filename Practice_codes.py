74ec6e5171085b0437be23cb0f9770db7388e9f8
tanishsolanki20 <tanish.solanki20@gmail.com>
2026-09-17 19:14:14 +0530

    Add Students class example with name and age attributes

---
+++ Practice_codes.py
@@ -0,0 +1,22 @@
+"""class Dog:
+    def __init__(self, name, breed, age):
+        self.name=name
+        self.breed=breed
+        self.age=age
+    def bark(self):
+        print(f"{self.name} says woof .")
+
+d=Dog("Cookie", "Shitzu", 2)
+d.bark()
+print(d.breed)"""
+
+class Students:
+    def __init__(self, name, age):
+        self.name=name
+        self.age=age
+
+    def students_names(self):
+        print(f"My name is {self.name}")
+
+s1=Students("Tanish", 13)
+s1.students_names()
